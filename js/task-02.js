2fb43e5f6ae17bf93eab4f7d8e455b952d8a30fa
Nata-Curly <nkucheryava@gmail.com>
2023-03-30 13:22:45 +0200

    hw-06_task-02

--- js/task-02.js
+++ js/task-02.js
@@ -7,17 +7,28 @@ const ingredients = [
   'Condiments',
 ];
 
+// const list = document.querySelector('#ingredients');
+
+// ingredients.forEach((ingredient) => {
+//   const li = document.createElement('li');
+//   li.classList.add('item');
+//   li.textContent = ingredient;
+  
+//   console.log(li);
+//   list.append(li);
+// });
+
 const list = document.querySelector('#ingredients');
 
-ingredients.forEach((ingredient) => {
-  const li = document.createElement('li');
-  li.classList.add('item');
-  li.textContent = ingredient;
-  
-  console.log(li);
-  list.append(li);
-});
-
+const markup = 
+  ingredients.map((ingredient) => {
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.textContent = ingredient;
+    return li;
+  });
+console.log(markup);
+list.append(...markup);
 
 // Напиши скрипт, который для каждого элемента массива ingredients:
 
